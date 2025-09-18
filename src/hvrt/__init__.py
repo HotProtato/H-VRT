d6761b9e3502671bfd7aa6ae5304a39e306f26bb
HotProtato <majorskillage@outlook.com>
2025-09-18 14:54:48 +1000

    Version corrected.

--- src/hvrt/__init__.py
+++ src/hvrt/__init__.py
@@ -1,4 +1,4 @@
-from .partitioner import HVRT_Partitioner
+from .partitioner import HVRTPartitioner
 from .metrics import calculate_feature_hhi_metric
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
