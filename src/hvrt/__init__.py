a87bb0c0c525c6064739865bc7d0086ceb6b7191
HotProtato <majorskillage@outlook.com>
2025-09-22 19:36:07 +1000

    Imports contextualized.

--- src/hvrt/__init__.py
+++ src/hvrt/__init__.py
@@ -1,23 +1,3 @@
 from .partitioner import HVRTPartitioner
-from .analysis import DistributionAnalyzer
-from ..metrics import (
-    full_report,
-    calculate_feature_hhi_metric,
-    PartitionProfiler,
-    FeatureReport,
-    SpanReport,
-    VarianceReport,
-)
 
-__version__ = "0.3.0"
-
-__all__ = [
-    "HVRTPartitioner",
-    "DistributionAnalyzer",
-    "full_report",
-    "calculate_feature_hhi_metric",
-    "PartitionProfiler",
-    "FeatureReport",
-    "SpanReport",
-    "VarianceReport",
-]
+__all__ = ["HVRTPartitioner"]
