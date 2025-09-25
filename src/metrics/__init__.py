2c3e14ed2fe35788592d26b867325b3883449378
HotProtato <majorskillage@outlook.com>
2025-09-25 17:25:52 +1000

    Updated to 0.5.0

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -2,7 +2,7 @@ from src.metrics.metrics import full_report, calculate_feature_hhi_metric
 from src.metrics.partition_profiler import PartitionProfiler
 from src.metrics.feature_data import FeatureReport, SpanReport, VarianceReport
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 __all__ = [
     "full_report",
